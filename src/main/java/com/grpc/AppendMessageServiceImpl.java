4bae21a63a24b424307285f6c9ef1f493e574dc1
Oksana Semenkova <ext-oksana.semenkova@here.com>
2021-11-12 11:49:57 +0200

    2-iteration-CountDownLatch was added

--- src/main/java/com/grpc/AppendMessageServiceImpl.java
+++ src/main/java/com/grpc/AppendMessageServiceImpl.java
@@ -3,7 +3,6 @@ package com.grpc;
 import java.util.Map;
 import java.util.concurrent.ConcurrentNavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import io.grpc.stub.StreamObserver;
@@ -12,17 +11,17 @@ import net.devh.boot.grpc.server.service.GrpcService;
 @GrpcService
 public class AppendMessageServiceImpl extends com.grpc.AppendMessageServiceGrpc.AppendMessageServiceImplBase {
 
-  private ConcurrentNavigableMap<Long, Integer> messages = new ConcurrentSkipListMap<>();
+  private ConcurrentNavigableMap<Long, String> messages = new ConcurrentSkipListMap<>();
 
   @Override
   public void append(com.grpc.LogMessage request,
                                                      StreamObserver<com.grpc.LogMessageAck> responseObserver) {
     try {
-      Thread.sleep(10000);
+      Thread.sleep(4000);
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
-    messages.put(request.getId(), request.getW());
+    messages.put(request.getId(), "write_concern=" + request.getW() + ", msg= " + request.getMsg());
 
     com.grpc.LogMessageAck ack = com.grpc.LogMessageAck.newBuilder()
                                                                      .setId(request.getId())
@@ -32,9 +31,9 @@ public class AppendMessageServiceImpl extends com.grpc.AppendMessageServiceGrpc.
     responseObserver.onCompleted();
   }
 
-  private String convertWithStream(Map<Long, Integer> map) {
+  private String convertWithStream(Map<Long, String> map) {
     String mapAsString = map.keySet().stream()
-                            .map(key -> key + "=" + map.get(key))
+                            .map(key -> map.get(key))
                             .collect(Collectors.joining(", ", "{", "}"));
     return mapAsString;
   }
